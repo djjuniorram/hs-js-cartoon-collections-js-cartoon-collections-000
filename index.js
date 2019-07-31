06af167919cf9eacab40d499e2657daa0758a63c
Ramesh Singh <rameshsingh.jr@gmail.com>
2019-07-31 09:14:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function dwarfRollCall(dwarves) {
   let dwarves_string = [];
-  for (let i = 0; i < dwarves.length; i++) {
+  let halfLength = dwarves.length/2;
+  for (let i = halfLength; i < dwarves.length; i++) {
     dwarves_string.push(`${i+1}. ${dwarves[i]} `);
   }
   return dwarves_string.join("");
@@ -27,8 +28,27 @@ function findTheCheese (foods) {
   for (var i = 0; i < foods.length; i++) {
     if (foods[i] == "cheddar") {
       return foods[i];
-    } else {
+    } else if (foods[i] == "swiss") {
+      return foods[i];
     }
   }
   return "no cheese!";
 }
+
+
+function wordsThatStartWithB(words){
+  var bWords = [];
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].startsWith("b")) {
+      bWords.push(words[i])
+    }
+  }
+  return bWords
+}
+
+"eric".toUpperCase() // ERIC
+"eric".startsWith("b") // false
+"bob".startsWith("b") // true
+
+wordsThatStartWithB(['bob', 'billy', 'joe']) // ['bob', 'billy']
+wordsThatStartWithB(['banana', 'pineapple', 'apple']) // ['banana']
